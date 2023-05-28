e0a1073bdb4c34dc2bda54378368900fd8e360a6
Emma Cadavra <emmacadavra@gmail.com>
2023-05-28 12:06:33 +0000

    Add comments to js file breaking down each step to return to later

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Start game
+
+// Choose question (random)
+
+// Display question with 4 potential answers
+
+// User clicks answer
+
+// Check the answer
+
+// If correct, answer turns green and increases correct score
+
+// If incorrect, answer turns red and increases incorrect score
+
+// Display next question (up to 10?)
+
+// After final question, display final score with message depending on score
+
+// Restart the game
