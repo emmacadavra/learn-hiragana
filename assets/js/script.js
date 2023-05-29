1426b1dd9ef5eee18640957c9fb7442e87284ebd
Emma Cadavra <emmacadavra@gmail.com>
2023-05-29 14:16:37 +0000

    Add comments to js file for hiragana and katakana, begin ja file with event listener waiting for page to load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,44 @@
-// Start game
+// Wait for page to finish loading
+document.addEventListener("DOMContentLoaded", function () {
 
-// Choose question (random)
+        }
 
-// Display question with 4 potential answers
+        // Hiragana: Start game
 
-// User clicks answer
+        // Hiragana: Choose question (random)
 
-// Check the answer
+        // Hiragana: Display question with 4 potential answers
 
-// If correct, answer turns green and increases correct score
+        // Hiragana: User clicks answer
 
-// If incorrect, answer turns red and increases incorrect score
+        // Hiragana: Check the answer
 
-// Display next question (up to 10?)
+        // Hiragana: If correct, answer turns green and increases correct score
 
-// After final question, display final score with message depending on score
+        // Hiragana: If incorrect, answer turns red and increases incorrect score
 
-// Restart the game
+        // Hiragana: Display next question (up to 10?)
+
+        // Hiragana: After final question, display final score with message depending on score
+
+        // Hiragana: Restart the game
+
+        // Katakana: Start game
+
+        // Katakana: Choose question (random)
+
+        // Katakana: Display question with 4 potential answers
+
+        // Katakana: User clicks answer
+
+        // Katakana: Check the answer
+
+        // Katakana: If correct, answer turns green and increases correct score
+
+        // Katakana: If incorrect, answer turns red and increases incorrect score
+
+        // Katakana: Display next question (up to 10?)
+
+        // Katakana: After final question, display final score with message depending on score
+
+        // Katakana: Restart the game
