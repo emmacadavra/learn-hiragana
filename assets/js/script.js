4e8556e99bf801cec765fdadd31b4eb8a5ca46ac
Emma Cadavra <emmacadavra@gmail.com>
2023-05-29 14:26:16 +0000

    Start adding hiragana objects to 'hiragana' array (copy and paste x46 then populate)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,54 @@
 // Wait for page to finish loading
 document.addEventListener("DOMContentLoaded", function () {
+    // Start screen?
+});
 
-        }
+// Hiragana: Start game
+const hiragana = [{
+    character: "あ",
+    phonetic: "a"
+}, {
+    character: "か",
+    phonetic: "ka"
+}, {
+    character: "",
+    phonetic: ""
+}]
 
-        // Hiragana: Start game
+// Hiragana: Choose question (random)
 
-        // Hiragana: Choose question (random)
+// Hiragana: Display question with 4 potential answers
 
-        // Hiragana: Display question with 4 potential answers
+// Hiragana: User clicks answer
 
-        // Hiragana: User clicks answer
+// Hiragana: Check the answer
 
-        // Hiragana: Check the answer
+// Hiragana: If correct, answer turns green and increases correct score
 
-        // Hiragana: If correct, answer turns green and increases correct score
+// Hiragana: If incorrect, answer turns red and increases incorrect score
 
-        // Hiragana: If incorrect, answer turns red and increases incorrect score
+// Hiragana: Display next question (up to 10?)
 
-        // Hiragana: Display next question (up to 10?)
+// Hiragana: After final question, display final score with message depending on score
 
-        // Hiragana: After final question, display final score with message depending on score
+// Hiragana: Restart the game
 
-        // Hiragana: Restart the game
+// Katakana: Start game
 
-        // Katakana: Start game
+// Katakana: Choose question (random)
 
-        // Katakana: Choose question (random)
+// Katakana: Display question with 4 potential answers
 
-        // Katakana: Display question with 4 potential answers
+// Katakana: User clicks answer
 
-        // Katakana: User clicks answer
+// Katakana: Check the answer
 
-        // Katakana: Check the answer
+// Katakana: If correct, answer turns green and increases correct score
 
-        // Katakana: If correct, answer turns green and increases correct score
+// Katakana: If incorrect, answer turns red and increases incorrect score
 
-        // Katakana: If incorrect, answer turns red and increases incorrect score
+// Katakana: Display next question (up to 10?)
 
-        // Katakana: Display next question (up to 10?)
+// Katakana: After final question, display final score with message depending on score
 
-        // Katakana: After final question, display final score with message depending on score
-
-        // Katakana: Restart the game
+// Katakana: Restart the game
